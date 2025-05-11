242527bc41b628ef4ed08a58f4f7806bc0d89a88
Clayton <ccrenshaw2012@gmail.com>
2025-05-10 21:17:40 -0500

    added brutalist logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
 export function Navbar() {
     return (
-        <nav class="bg-red-800 shadow-md px-4 py-3 flex items-center justify-between">
-            <div class="text-xl font-semibold text-white">
-                <a href="/home/dashboard" class="hover:text-gray-600">Kallax</a>
+        <nav className="bg-red-900 shadow-md px-4 py-3 flex items-center justify-between">
+            <div className="text-xl font-semibold text-white">
+                <a href="/home/dashboard" className="hover:text-gray-600">
+                    <img src="/navbar_logo.png" alt="Logo" width="50px" height="50px"/>
+                </a>
             </div>
 
-            <div class="hidden md:flex space-x-6 text-white">
-                <a href="/library/home" class="hover:text-gray-600">Library</a>
-                <a href="/tags" class="hover:text-gray-600">Discover</a>
-                <a href="/import" class="hover:text-gray-600">Profile</a>
+            <div className="hidden md:flex space-x-6 text-white">
+                <a href="/library/home" className="hover:text-gray-600">Library</a>
+                <a href="/tags" className="hover:text-gray-600">Discover</a>
+                <a href="/import" className="hover:text-gray-600">Profile</a>
             </div>
         </nav>
     );
